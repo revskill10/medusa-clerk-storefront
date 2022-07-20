f54f0431971c976acf6edfcf0b5802c336070696
Truong Dung <truonghoangdung1987hp@gmail.com>
2022-07-20 17:32:43 +0700

    Fix

--- containers/ClerkSignIn.js
+++ containers/ClerkSignIn.js
@@ -5,12 +5,10 @@ import { client } from "../lib/client";
 const ClerkSignedIn = ({ children }) => {
     const auth = useAuth()
     useEffect(() => {
-        if (auth.isSignedIn) {
-            client.post('/store/clerk/auth', {})
-            .then(console.log)
-            .catch(console.log)
-        }
-    }, [auth.isSignedIn])
+        client.post('/store/clerk/auth', {})
+        .then(console.log)
+        .catch(console.log)
+    }, [])
     return children
 }
 
